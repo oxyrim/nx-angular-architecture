c96bd24c2f5d07c2d8de1f1466aa39f5cf32b2a5
oxyrim <thukten73@gmail.com>
2025-04-13 19:48:40 -0400

    add routes

--- apps/finance/expenses-registration/src/app/app.routes.ts
+++ apps/finance/expenses-registration/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Route } from '@angular/router';
+import { ExpensesOverviewPageComponent } from './pages/expenses-overview-page/expenses-overview-page.component';
+import { ExpensesApprovalPageComponent } from './pages/expenses-approval-page/expenses-approval-page.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  { path: 'expenses-overview', component: ExpensesOverviewPageComponent },
+  { path: 'expenses-approval', component: ExpensesApprovalPageComponent },
+];
