546779b749aa1cb43dd111d14219a2d124bb1444
oxyrim <thukten73@gmail.com>
2025-04-13 19:56:52 -0400

    add title resolver

--- apps/finance/expenses-registration/src/app/app.routes.ts
+++ apps/finance/expenses-registration/src/app/app.routes.ts
@@ -1,8 +1,20 @@
-import { Route } from '@angular/router';
+import { ActivatedRouteSnapshot, ResolveFn, Route } from '@angular/router';
 import { ExpensesOverviewPageComponent } from './pages/expenses-overview-page/expenses-overview-page.component';
 import { ExpensesApprovalPageComponent } from './pages/expenses-approval-page/expenses-approval-page.component';
 
+export const titleResolver: ResolveFn<string> = (
+  route: ActivatedRouteSnapshot
+) => route.routeConfig?.path?.replace('-', ' ') ?? '';
+
 export const appRoutes: Route[] = [
-  { path: 'expenses-overview', component: ExpensesOverviewPageComponent },
-  { path: 'expenses-approval', component: ExpensesApprovalPageComponent },
+  {
+    path: 'expenses-overview',
+    component: ExpensesOverviewPageComponent,
+    title: titleResolver,
+  },
+  {
+    path: 'expenses-approval',
+    component: ExpensesApprovalPageComponent,
+    title: titleResolver,
+  },
 ];
