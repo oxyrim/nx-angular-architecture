d8b5c97e8d938cd26953f37404e585359e8c137e
oxyrim <thukten73@gmail.com>
2025-04-15 11:37:29 -0400

    add custom pipe

--- apps/finance/expenses-registration/src/app/pages/expenses-overview-page/expenses-overview-page.component.ts
+++ apps/finance/expenses-registration/src/app/pages/expenses-overview-page/expenses-overview-page.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MultiplyPipe } from '@ng-libs/shared/util/common-pipes';
 
 @Component({
   selector: 'app-expenses-overview-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MultiplyPipe],
   templateUrl: './expenses-overview-page.component.html',
   styleUrl: './expenses-overview-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
